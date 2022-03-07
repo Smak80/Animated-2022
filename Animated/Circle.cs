7c255ea29c9cee7e72ba8598ae32d62f35a1c6bd
Sergey Makletsov <smaklets@kpfu.ru>
2022-03-07 12:49:10 +0300

    Добавьте файлы проекта.

---
+++ Animated/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animated
+{
+    public class Circle
+    {
+        private Point _pos;
+        private int _diameter;
+        private int _dx = 3;
+        private Size _containerSize;
+        public Color Color { get; set; }
+
+        public Circle(Size containerSize)
+        {
+            _pos = new Point(0, 0);
+            Color = Color.Blue;
+            _diameter = 100;
+            _containerSize = containerSize;
+        }
+
+        public void Paint(Graphics g)
+        {
+            var b = new SolidBrush(Color);
+            g.FillEllipse(b, _pos.X, _pos.Y, _diameter, _diameter);
+        }
+
+        public bool Move()
+        {
+            _pos.X += _dx;
+            return _pos.X < _containerSize.Width - _diameter;
+        }
+    }
+}
